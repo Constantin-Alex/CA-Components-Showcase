47ba2001e7bd3dd981461a5f60aecdc12dafd340
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2024-06-27 11:15:32 +0100

    Components showcase

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon()],
+  site: "https://constantin-alex.github.io",
+  base: "CA-Components-Showcase",
+});
+
